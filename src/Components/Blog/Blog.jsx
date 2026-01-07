ca97424baf0a1449a894354d0236abcb67425669
Shafkatahmad <shafkataahmad@gmail.com>
2026-01-07 16:46:55 +0600

    title, photos, hashtags

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,8 +1,26 @@
 const Blog = ({blog}) => {
-  const {title} = blog
+  const {title, cover,author, author_img, reading_time, posted_data, hashtags} = blog
   return (
     <div>
-      <h2 className="text-4xl">{title}</h2>
+      <img src={cover} alt={`Cover Picture of the title ${title}`} />
+      <div className="flex justify-between">
+        <div className="flex gap-6">
+          <img className="w-14 h-14 rounded-full" src={author_img} alt="" />
+          <div>
+            <h4 className="text-2xl">{author}</h4>
+            <p>{posted_data}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl font-bold">{title}</h2>
+      <p>
+        {
+          hashtags.map((hash, idx) => <span key={idx}><a className="mr-4" href="">#{hash}</a></span>)
+        }
+      </p>
     </div>
   );
 };
